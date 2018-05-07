de779dd0b6936a8672c3720f1091a800912f9d0e
liqiqi <liqiqi@1076.com>
2018-05-07 09:53:33 +0800

    给头部加上返回按钮

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
 
-    <mt-header fixed title="叮叮当购物商城"></mt-header>
+    <mt-header fixed title="叮叮当购物商城">
+    	<mt-button icon="back" slot="left" v-if="$route.path!='/home'" @click='$router.go(-1)'>返回</mt-button>
+		</mt-header>
 
     <router-view/>
 		
--- src/components/news/list.vue
+++ src/components/news/list.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-      <h1>新闻列表页</h1>
+  <div class="page">
+
+      <ul class="mui-table-view mui-table-view-chevron">
+				<li class="mui-table-view-cell mui-media">
+					<a class="mui-navigate-right">
+						<img class="mui-media-object mui-pull-left" src="../../assets/images/menu1.png">
+						<div class="mui-media-body">
+							CBD
+							<p class='mui-ellipsis'>烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a class='mui-navigate-right' href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="../../assets/images/menu1.png">
+						<div class="mui-media-body">
+							远眺
+							<p class='mui-ellipsis'>静静的看这个世界，最后终于疯了</p>
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a class="mui-navigate-right">
+						<img class="mui-media-object mui-pull-left" src="../../assets/images/menu1.png">
+						<div class="mui-media-body">
+							幸福
+							<p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+						</div>
+					</a>
+				</li>
+			</ul>
+      
   </div>
 </template>
 
